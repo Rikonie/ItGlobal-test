90ccfa9b303dd18d21688361465e4113757ffe2b
Anatoly Kozachenko <anatoly.kozachenko@arcadia.spb.ru>
2021-10-20 13:34:36 +0300

    Initial localization. Added i18n.

---
+++ src/localization/localization.js
@@ -0,0 +1,15 @@
+const localizationData ={
+    resources: {
+        en: {
+            translation: {"Welcome to React": "Welcome to React and react-i18next"}
+        },
+        ru: {
+            translation: {"Welcome to React": "Привет локализация"}
+        }
+    },
+    lng: "en",
+        fallbackLng: "en",
+    interpolation: {escapeValue: false}
+}
+
+export default localizationData;
